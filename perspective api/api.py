725f050bc181f98c5e78cf6ae72bd2dc49fed79e
unknown <ccmilne@umich.edu>
2021-10-07 11:00:08 -0400

    Placing Google API keys in .gitignore

--- perspective api/api.py
+++ perspective api/api.py
@@ -2,33 +2,34 @@ from googleapiclient import discovery
 import json
 import os
 
-#Testing connection
-def implicit():
-    from google.cloud import storage
+# # # Testing connection
+# def implicit():
+#     from google.cloud import storage
 
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
+#     # If you don't specify credentials when constructing the client, the
+#     # client library will look for credentials in the environment.
+#     storage_client = storage.Client()
 
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
+#     # Make an authenticated API request
+#     buckets = list(storage_client.list_buckets())
+#     print(buckets)
 
-# API_KEY = os.getenv('PERSPECTIVE_API_KEY')
-# print(API_KEY)
+#API_KEY = os.getenv('PERSPECTIVE_API_KEY')
+API_KEY = 'AIzaSyBzEWVEOBOZCXY3fE_J_OJapY5QTES2J2k'
+print(API_KEY)
 
-# client = discovery.build(
-#   "commentanalyzer",
-#   "v1alpha1",
-#   developerKey=API_KEY,
-#   discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
-#   static_discovery=False,
-# )
+client = discovery.build(
+  "commentanalyzer",
+  "v1alpha1",
+  developerKey=API_KEY,
+  discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+  static_discovery=False,
+)
 
-# analyze_request = {
-#   'comment': { 'text': 'friendly greetings from python' },
-#   'requestedAttributes': {'TOXICITY': {}}
-# }
+analyze_request = {
+  'comment': { 'text': 'friendly greetings from python' },
+  'requestedAttributes': {'TOXICITY': {}}
+}
 
-# response = client.comments().analyze(body=analyze_request).execute()
-# print(json.dumps(response, indent=2))
+response = client.comments().analyze(body=analyze_request).execute()
+print(json.dumps(response, indent=2))
