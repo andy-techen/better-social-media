fa6769711356de2e92acb81ffb6c58bd5cbb8e0b
Andy Chen <andy27479@gmail.com>
2021-10-28 09:55:46 -0400

    trying to link to main.js

--- dist/background.js
+++ dist/background.js
@@ -1,6 +1,7 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.tabs.onActivated.addListener(tab => {
+  chrome.tabs.get(tab.tabId, tab_info => {
+    if(/^https:\/\/twitter\.com\/search/.test(tab_info.url)) {
+      chrome.tabs.executeScript(null, {file: 'main.js'}, () => console.log("Retrieved query!"))
+    }
+  })
 });
