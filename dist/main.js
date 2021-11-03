f3f0044d33a5d8321dc5f05e4d6d13b8c5cc818c
Andy <andy27479@gmail.com>
2021-11-03 17:25:31 -0400

    testing interaction with tweets

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{const e=document.getElementsByClassName("filter-button");chrome.storage.sync.get("query",(({query:e})=>{document.querySelectorAll("input[aria-label='Search query']")[0].value=e,console.log("Query is currently "+e)})),e.addEventListener("click",(()=>{console.log("Abracadabra!")}))})();
