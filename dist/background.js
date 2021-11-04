3ea7594463234d8c8d8c40c79929e68d0a0f13dd
Andy <andy27479@gmail.com>
2021-11-04 12:48:43 -0400

    reformating background functionalities

--- dist/background.js
+++ dist/background.js
@@ -1,3 +1,9 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({hide: true}, function() {
+    console.log("Hide image is on");
+  });
+});
+
 chrome.tabs.onActivated.addListener(tab => {
   chrome.tabs.get(tab.tabId, tab_info => {
     if(/^https:\/\/twitter\.com\/search/.test(tab_info.url)) {
