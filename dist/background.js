131dd32bb6069af7e9b4c615d1d49af255804c13
Andy Chen <andy27479@gmail.com>
2021-11-27 18:51:55 -0500

    set query on install

--- dist/background.js
+++ dist/background.js
@@ -1,8 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({depressive: 0.8});
-  chrome.storage.sync.set({toxic: 0.5});
-  chrome.storage.sync.set({sexual: 0.25});
-  chrome.storage.sync.set({profanity: 0.1});
+  chrome.storage.sync.set({"depressive": 0.8});
+  chrome.storage.sync.set({"toxic": 0.5});
+  chrome.storage.sync.set({"sexual": 0.25});
+  chrome.storage.sync.set({"profanity": 0.1});
+  chrome.storage.sync.set({"query": ""});
 });
 
 chrome.declarativeContent.onPageChanged.removeRules(() => {
@@ -11,6 +12,6 @@ chrome.declarativeContent.onPageChanged.removeRules(() => {
       pageUrl: {hostEquals: 'twitter.com'}
     })
   ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
+    actions: [new chrome.declarativeContent.ShowPageAction()]
   }]);
 });
