1a466f0be15abbb8a4c780bab9f3801d1eb0e4d5
Andy Chen <andy27479@gmail.com>
2021-12-02 11:49:56 -0500

    testing depressive post request

--- data/ModelAPI/api.py
+++ data/ModelAPI/api.py
@@ -1,15 +1,33 @@
 from flask import Flask, request, jsonify
 import numpy as np
+import pandas as pd
 import pickle
-import json
-import os
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+nltk.download('punkt')
+nltk.download('stopwords')
 
 app = Flask(__name__)
+vectorizer = TfidfVectorizer(vocabulary=pickle.load(open("feature.pickle", "rb")))
 
-@app.route('/depressive/', methods=['POST'])
+def preprocess(tweet):
+    processed_punc = re.sub(r'[^@a-zA-Z ]', '', tweet)
+    processed = re.sub('https:\S+|@\S+|\b\w{,1}\b', '', processed_punc)
+    tokens = word_tokenize(processed)
+    tokens_sw = [token for token in tokens if not token in stopwords.words()]
+    return tokens_sw
+
+@app.route('/depressive', methods=['POST'])
 def get_depressive():
-    data = request.get_json()
-    prediction = np.array2string(depressive.predict(data))
+    tweet = request.get_json()
+    print(tweet['tweet'])
+    print(type(tweet['tweet']))
+    x_train = preprocess(tweet['tweet'])
+    x_train_tokenized = vectorizer.transform(x_train)
+    prediction = depressive.predict(x_train_tokenized)
 
     return jsonify(prediction)
 
@@ -23,4 +41,4 @@ def get_depressive():
 if __name__ == '__main__':
     depressive = pickle.load(open('data/ModelAPI/models/depressive_xgb.pickle', 'rb'))
     # toxic = pickle.load(open('toxic.pickle', 'rb'))
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
