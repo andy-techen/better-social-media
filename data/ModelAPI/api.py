e9fdc3966fb69e9a17566bcd275298a1d32712db
Andy <andy27479@gmail.com>
2021-12-04 19:36:41 -0500

    finished depressive api post request

--- data/ModelAPI/api.py
+++ data/ModelAPI/api.py
@@ -4,6 +4,8 @@ import pandas as pd
 import pickle
 import re
 import nltk
+import xgboost
+from gensim.parsing.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,32 +13,30 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 app = Flask(__name__)
-vectorizer = TfidfVectorizer(vocabulary=pickle.load(open("feature.pickle", "rb")))
+vectorizer_depressive = pickle.load(open("data/ModelAPI/vectorizers/vectorizer_depressive.pickle", "rb"))
+vectorizer_perspective = pickle.load(open("data/ModelAPI/vectorizers/PerspectiveAPI_vectorizer.pickle", "rb"))
+p = PorterStemmer()
 
 def preprocess(tweet):
     processed_punc = re.sub(r'[^@a-zA-Z ]', '', tweet)
-    processed = re.sub('https:\S+|@\S+|\b\w{,1}\b', '', processed_punc)
+    processed = re.sub('https\S+|@\S+|\b\w{,1}\b', '', processed_punc)
     tokens = word_tokenize(processed)
     tokens_sw = [token for token in tokens if not token in stopwords.words()]
-    return tokens_sw
+    sentence = ' '.join(tokens_sw)
+    stemmed_sentence = p.stem_sentence(sentence)
+
+    return stemmed_sentence
 
 @app.route('/depressive', methods=['POST'])
 def get_depressive():
     tweet = request.get_json()
-    print(tweet['tweet'])
-    print(type(tweet['tweet']))
     x_train = preprocess(tweet['tweet'])
-    x_train_tokenized = vectorizer.transform(x_train)
-    prediction = depressive.predict(x_train_tokenized)
+    x_train_tokenized = vectorizer_depressive.transform([x_train])
+    prediction = depressive.predict_proba(x_train_tokenized)[0][1]
 
-    return jsonify(prediction)
+    return jsonify(prediction=str(prediction))
 
-# @app.route('/toxic/', methods=['POST'])
-# def get_toxic():
-#     data = request.get_json()
-#     prediction = np.array2string(toxic.predict(data))
 
-#     return jsonify(prediction)
 
 if __name__ == '__main__':
     depressive = pickle.load(open('data/ModelAPI/models/depressive_xgb.pickle', 'rb'))
