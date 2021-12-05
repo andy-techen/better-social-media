322d13ef8938b2f808b9c699075e0394a96eb526
Andy <andy27479@gmail.com>
2021-12-05 01:00:38 -0500

    set up post requests

--- dist/scripts/twitter.js
+++ dist/scripts/twitter.js
@@ -2,13 +2,19 @@ var queryBar = document.querySelectorAll("input[aria-label='Search query']")[0];
 var query = queryBar.value;
 var tweetsDiv = document.querySelector('div[aria-label="Timeline: Search timeline"]');
 var tweets = tweetsDiv.getElementsByTagName("article");
-const req = new XMLHttpRequest();
-const url_d = "https://better-social-media.herokuapp.com/api/depressive";
-const url_p = "https://better-social-media.herokuapp.com/api/perspective";
 var depressive = "";
 var toxic = "";
 var sexual = "";
 var profanity = "";
+
+const req_d = new XMLHttpRequest();
+const req_p = new XMLHttpRequest();
+const url_d = "https://better-social-media.herokuapp.com/api/depressive";
+const url_p = "https://better-social-media.herokuapp.com/api/perspective";
+req_d.open("POST", url_d, true);
+req_p.open("POST", url_p, true);
+req_d.setRequestHeader("Content-type", "application/json");
+req_p.setRequestHeader("Content-type", "application/json");
 
 chrome.storage.sync.get("depressive", (res) => {
     depressive = res.depressive;
@@ -21,7 +27,7 @@ chrome.storage.sync.get("sexual", (res) => {
 });
 chrome.storage.sync.get("profanity", (res) => {
     profanity = res.profanity;
-});
+})
 
 queryBar.addEventListener("change", () => {
     var query = queryBar.value;
