55a9e85912cf277c33195c236530234a43374fa7
Andy Chen <andy27479@gmail.com>
2021-12-10 01:06:01 -0500

    testing fetch post request, cors settings in flask

--- dist/scripts/twitter.js
+++ dist/scripts/twitter.js
@@ -7,14 +7,8 @@ var toxic = "";
 var sexual = "";
 var profanity = "";
 
-const req_d = new XMLHttpRequest();
-const req_p = new XMLHttpRequest();
-const url_d = "https://better-social-media.herokuapp.com/api/depressive";
-const url_p = "https://better-social-media.herokuapp.com/api/perspective";
-req_d.open("POST", url_d, true);
-req_p.open("POST", url_p, true);
-req_d.setRequestHeader("Content-type", "application/json");
-req_p.setRequestHeader("Content-type", "application/json");
+var url_d = "https://better-social-media.herokuapp.com/api/depressive";
+var url_p = "https://better-social-media.herokuapp.com/api/perspective";
 
 chrome.storage.sync.get("depressive", (res) => {
     depressive = res.depressive;
@@ -39,6 +33,21 @@ queryBar.addEventListener("change", () => {
 tweetsDiv.addEventListener("DOMSubtreeModified", () => {
     for (let i = 0; i < tweets.length; i++) {
         let tweet = tweets[i].querySelectorAll("div[class='css-1dbjc4n r-18u37iz']")[1];
+        fetch(url_d, {
+            method: 'post',
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({'tweet': tweet.textContent}),
+            mode: 'no-cors'
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log('Request succeeded with JSON response', data);
+        })
+        .catch((err) => {
+            console.log('Request failed', err);
+        });
         if (tweet.textContent.includes("@umich")) {
             tweets[i].style.opacity = 0.1;
         }
